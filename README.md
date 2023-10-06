3dbb515c2c2bb53c98ebd1d6179219478d529e54
Iulian Carnaru <iulian.carnaru@gmail.com>
2023-10-06 21:16:48 +0100

    orders microservice with redis

---
+++ README.md
@@ -0,0 +1,1 @@
+docker run --name orders-redis -p 6379:6379 -d redis
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/iuliancarnaru/go-orders-api/application"
+)
+
+func main() {
+	app := application.New()
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer cancel()
+
+	err := app.Start(ctx)
+	if err != nil {
+		fmt.Println("failed to start the app:", err)
+	}
+}
